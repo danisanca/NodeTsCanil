acd1a8e647f0e8d360d3e37f841e370807280123
Daniel Mendonça <dani_sanca@hotmail.com>
2022-02-19 09:51:19 -0300

    Finalização projeto Canil

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import router from './routes/index';
+
+const server = express();
+
+dotenv.config();
+
+server.set('view engine','mustache');
+server.set('views',path.join(__dirname,'views'));
+server.engine('mustache',mustache());
+server.use(express.static(path.join(__dirname, "../public")));
+
+//Rotas
+server.use(router);
+
+server.use((req,res)=>{
+    res.render('pages/404');
+})
+
+server.listen(process.env.PORT,()=>{
+    console.log("Servidor Rodando");
+})
